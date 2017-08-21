a9db4f62a4ac3effd60015c1bc038a7f696572dc
aa <bb>
2017-08-21 17:34:39 +0800

    添加了一个方法

--- testgit/src/first/first.java
+++ testgit/src/first/first.java
@@ -5,5 +5,6 @@ public class first {
 	     System.out.println("小名是个2 比");
 	     System.out.println("sdfsdfds");
 	     System.out.println("使得发达省份");
+	     System.out.println("斯蒂芬是范德萨忿忿地说阿凡达岁");
 	}
 }
